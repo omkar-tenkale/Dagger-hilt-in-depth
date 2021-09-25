5bae7c983d123650d436a01bf23030fa6478a527
Devrath <devrath.dev595@gmail.com>
2021-09-25 22:33:20 +0530

    Create Student.kt

---
+++ basics/Implnheritence/app/src/main/java/com/dependency/code/Student.kt
@@ -0,0 +1,13 @@
+package com.dependency.code
+
+class Student(val name:String) {
+
+    fun study(topic:String){
+        println("$name is studying $topic")
+    }
+
+    fun think(){
+        println("$name is thinking")
+    }
+    
+}
